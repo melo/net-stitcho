0745aa3cda8a73513770e5113f0797008646d593
Pedro Melo <melo@simplicidade.org>
2008-10-17 15:25:10 +0100

    Basic usage POD, including constructor documentation
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- Net-Stitcho/lib/Net/Stitcho.pm
+++ Net-Stitcho/lib/Net/Stitcho.pm
@@ -15,7 +15,7 @@ __END__
 
 =head1 NAME
 
-Net::Stitcho - The great new Net::Stitcho!
+Net::Stitcho - Client module for the Stitcho.com API
 
 
 
@@ -27,22 +27,71 @@ Version 0.01
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
     use Net::Stitcho;
+    
+    my $api = Net::Stitcho->new({
+      id  => 1,          # your partner id
+      key => '3246327a', # the secrect signing key
+    });
+    
+    my $uri = $api->send_uri({
+      email   => 'melo@cpan.org',       # Recipient address
+      message => 'Yuppi!',              # Message to send
+      url     => 'http://stitcho.com/', # Jump to this URL on click
+      icon    => 1,                     # ID of the icon to use
+    });
 
-    my $foo = Net::Stitcho->new();
-    ...
+
+=head1 DESCRITION
+
+The module provides access to the Stitcho.com notification service using the
+provided HTTP-based API.
+
+You need to get an partner account at the site. After you have your API
+secret key, you can start to use this module.
+
+You can use this module in two ways:
+
+=over 4
+
+=item to generate the proper URIs to use with the API;
+
+=item to call the API;
+
+=back
+
+The first option is best if you have a way to do the HTTP GET request
+yourself. For example, if you are using AnyEvent or POE, you can use
+their Async HTTP client to call the API using the URI calculated by
+this module.
+
+
+
+=head1 METHODS
+
+=head2 new
+
+Creates a new API object. Accepts a hashref of parameters. Valid keys are:
+
+=over 4
+
+=item id
+
+Partner ID provided by Stitcho.com.
+
+=item key
+
+Secret signing key provided by Stitcho.com.
+
+=back
+
+Returns a API object.
 
 
 
 =head1 EXPORT
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
+This module does not export any symbols.
 
 
 =head1 AUTHOR
